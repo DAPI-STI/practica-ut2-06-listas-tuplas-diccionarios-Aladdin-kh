1fe1adc6b8dc13dd015f219e2ce4449b4a4b236b
Aladdin-kh <akhetto@educacion.navarra.es>
2026-01-19 11:38:44 +0100

    Tarea Completada

--- src/ex04_best_student.py
+++ src/ex04_best_student.py
@@ -15,12 +15,10 @@ def best_student(records: list[tuple[str, float]]) -> tuple[str, float]:
     """
     if not records:
         raise ValueError("La lista de registros está vacía.")
-    elif len(records) == 1:
-        return records[0]
     else:
         best = records[0]
-        for record in records:
-            if record[1] > best[1]:
-                best = record
+        for i in records:
+            if i[1] > best[1]:
+                best = i
         return best
     raise NotImplementedError("Implementa best_student(records)")
